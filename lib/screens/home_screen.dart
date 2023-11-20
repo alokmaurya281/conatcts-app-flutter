2f4838000175cea88aa996820c5f8125e35fcfbd
Alok Maurya <alok.maurya@w3villa.com>
2023-11-20 17:20:20 +0530

    done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,18 +24,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _requestPermission() async {
     await Permission.phone.request();
-    // await Permission..request();
-
-    // if (status.isGranted) {
-    //   // Permission granted, proceed with making the call
-    // } else {
-    //   // Permission denied, handle accordingly
-    // }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     data();
     _requestPermission();
@@ -44,127 +36,141 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Contacts'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 4),
-            child: Consumer<ThemeProvider>(
-              builder: (context, provider, child) {
-                return GestureDetector(
-                  onTap: () {
-                    provider.setTheme();
-                  },
-                  child: Icon(
-                    context.read<ThemeProvider>().isDark
-                        ? Icons.light_mode
-                        : Icons.dark_mode,
-                    size: 24,
-                  ),
-                );
-              },
-            ),
-          ),
-          const PopUpMenuWidget(),
-        ],
-      ),
+      appBar: appbar(),
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 50,
-            margin: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: const Color.fromARGB(144, 212, 212, 212)),
-            child: TextFormField(
-              style: const TextStyle(fontSize: 14),
-              decoration: const InputDecoration(
-                hintText: 'Search Contacts',
-                prefixIcon: Icon(Icons.search),
-                border: InputBorder.none,
-                enabledBorder: InputBorder.none,
-              ),
-            ),
-          ),
+          _searchField(),
           const SizedBox(
             height: 20,
           ),
-          Consumer<ContactsProvider>(
-            builder: (context, provider, child) {
-              final contactslist = provider.contactsList;
-              return Expanded(
-                child: provider.isLoading
-                    ? Shimmer.fromColors(
-                        period: const Duration(seconds: 2),
-                        baseColor: const Color.fromARGB(255, 140, 140, 140),
-                        highlightColor:
-                            const Color.fromARGB(255, 255, 255, 255),
-                        child: ListView.builder(
-                            itemCount: 10,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 16),
-                                child: Card(
-                                  elevation: 1,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: const Row(
-                                    children: [
-                                      SizedBox(
-                                        width: 60,
-                                        height: 60,
-                                        child: CircleAvatar(),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              );
-                            }),
-                      )
-                    : contactslist.length == 0
-                        ? Text('No contact found')
-                        : ListView.builder(
-                            itemCount: contactslist.length,
-                            itemBuilder: (context, index) {
-                              return ContactWidgetTile(
-                                image: 'image',
-                                name: contactslist[index].name,
-                                id: contactslist[index].id,
-                              );
-                            },
+          _contactsList(),
+        ],
+      ),
+      floatingActionButton: floatingbuttonaddcontact(context),
+    );
+  }
+
+  Consumer<ContactsProvider> _contactsList() {
+    return Consumer<ContactsProvider>(
+      builder: (context, provider, child) {
+        final contactslist = provider.contactsList;
+        return Expanded(
+          child: provider.isLoading
+              ? Shimmer.fromColors(
+                  period: const Duration(seconds: 2),
+                  baseColor: const Color.fromARGB(255, 140, 140, 140),
+                  highlightColor: const Color.fromARGB(255, 255, 255, 255),
+                  child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Card(
+                            elevation: 1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: const Row(
+                              children: [
+                                SizedBox(
+                                  width: 60,
+                                  height: 60,
+                                  child: CircleAvatar(),
+                                )
+                              ],
+                            ),
                           ),
+                        );
+                      }),
+                )
+              : contactslist.length == 0
+                  ? Text('No contact found')
+                  : ListView.builder(
+                      itemCount: contactslist.length,
+                      itemBuilder: (context, index) {
+                        return ContactWidgetTile(
+                          image: 'image',
+                          name: contactslist[index].name,
+                          id: contactslist[index].id,
+                        );
+                      },
+                    ),
+        );
+      },
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: const Color.fromARGB(144, 212, 212, 212)),
+      child: TextFormField(
+        style: const TextStyle(fontSize: 14),
+        decoration: const InputDecoration(
+          hintText: 'Search Contacts',
+          prefixIcon: Icon(Icons.search),
+          border: InputBorder.none,
+          enabledBorder: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  GestureDetector floatingbuttonaddcontact(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ContactFormScreen(),
+          ),
+        );
+      },
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color.fromARGB(255, 133, 18, 226),
+        ),
+        child: const Icon(
+          Icons.add,
+          color: Color.fromRGBO(215, 215, 215, 1),
+          size: 36,
+        ),
+      ),
+    );
+  }
+
+  AppBar appbar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Text('Contacts'),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 4),
+          child: Consumer<ThemeProvider>(
+            builder: (context, provider, child) {
+              return GestureDetector(
+                onTap: () {
+                  provider.setTheme();
+                },
+                child: Icon(
+                  context.read<ThemeProvider>().isDark
+                      ? Icons.light_mode
+                      : Icons.dark_mode,
+                  size: 24,
+                ),
               );
             },
           ),
-        ],
-      ),
-      floatingActionButton: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ContactFormScreen(),
-            ),
-          );
-        },
-        child: Container(
-          width: 50,
-          height: 50,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: Color.fromARGB(255, 133, 18, 226),
-          ),
-          child: const Icon(
-            Icons.add,
-            color: Color.fromRGBO(215, 215, 215, 1),
-            size: 36,
-          ),
         ),
-      ),
+        const PopUpMenuWidget(),
+      ],
     );
   }
 }
